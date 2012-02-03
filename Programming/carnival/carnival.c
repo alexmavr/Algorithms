66660c74f295002c811b1e4921bda2d23875bfd1
afein <nalfemp@gmail.com>
2012-02-03 07:26:40 +0200

    still buggy at second tree

--- Programming/carnival/carnival.c
+++ Programming/carnival/carnival.c
@@ -1,7 +1,4 @@
-/*TODO use the adjacency matrix to create the max table
-  TODO find minimum edge
-  TODO return tree without that edge
- 
+/*
  * =====================================================================================
  *
  *       Filename:  carnival.c
@@ -23,9 +20,10 @@
 #include <stdio.h> 
 long N;          /* Ari8mos koryfwn */
 long M;          /*  Ari8mos akmwn */
-long** W;                  /* Adjacency Matrix */
-long* E[3];             /*  Synolo twn Akmwn (E)  */ 
-long* U;       /* Union */
+long** W;        /* Adjacency Matrix */
+long* E[4];      /*  Synolo twn Akmwn (E)  */ 
+long* U;         /* Union Set   */
+long* E_M;       /* Edge Maximums */
 
 	/*	Synartiseis gia QuickSort		*/
 void swap(long  int *a, long  int *b)
@@ -35,6 +33,12 @@ void swap(long  int *a, long  int *b)
 long partition( long l,  long r) {
      long int pivot;
      long int i, j;
+     long k = (l+r)/2;
+     swap(&E[0][l],&E[0][k]);
+     swap(&E[1][l],&E[1][k]);
+     swap(&E[2][l],&E[2][k]);
+     swap(&E[3][l],&E[3][k]);
+
 	pivot = E[2][l];
 	i = l;
 	j = r + 1;
@@ -47,10 +51,12 @@ long partition( long l,  long r) {
 		swap(&E[0][i],&E[0][j]);
 		swap(&E[1][i],&E[1][j]);
 		swap(&E[2][i],&E[2][j]);
+		swap(&E[3][i],&E[3][j]);
 	}
      swap(&E[0][l],&E[0][j]);
      swap(&E[1][l],&E[1][j]);
      swap(&E[2][l],&E[2][j]);
+     swap(&E[3][l],&E[3][j]);
 	return j;
 }
 
@@ -65,11 +71,6 @@ void quicksort( long l,  long r) {
 }
 
      /*  Synartiseis gia kruskal's */
-void makeset(long i) {
-     U[i] = i;
-} 
-
-
 long find(long i) { // Kai sympti3i monopatiou
      long j;
      j = i;
@@ -78,110 +79,115 @@ long find(long i) { // Kai sympti3i monopatiou
      }
      return U[j];
 }
+void unite(long x, long y, long weight) 
+{
 
-void merge(long p,  long q) {
-     if (p < q) U[q] = p;
-     else U[p] = q;
+     long x_root, y_root;
+     if (x==y) return;
+     x_root=find(x);
+     y_root=find(y);
+     U[y_root]=x;       
+     if (E_M[x_root]<weight){
+          E_M[x_root]=weight;
+
+     }
+     if (E_M[y]<weight){
+          E_M[y]=weight;
+     }
 }
 
-
-void initialize(long n) { /*  Arxikopoihsh tou U */
+void initialize(long n) { /*  Arxikopoihsh tou U kai tou E_M */
      long i;
-     for (i = 0; i < n; i++)
-          makeset(i);
+     for (i = 0; i < n; i++){
+          U[i]=i;
+          E_M[i]=0;
+     }
 } 
 
 /*  Algori8mos tou Kruskal gia evresi MST */
-void kruskal() {
-     long F[N - 1][3];      /*  To MST */
+unsigned long long kruskal() {
      long edges = 0;      /*  akmes sto MST */
      long next = 0;      /*  Epomeni akmi pros elegxo */
-     long weight = 0;     /*  minimal spanning tree weight */
+     unsigned long long weight = 0;     /*  minimal spanning tree weight */
      long a,  b, i,  j; /*  counter/placeholder variables */
-    
-    quicksort(0, M);    /*   sort se au3ousa seira tis akmes */
-    printf("\n");
-     for (i = 0; i < M; i++) {
-          printf(" %lu",  E[0][i]);
-          printf(" %lu",  E[1][i]);
-          printf(" %lu",  E[2][i]);
-          printf("\n");
-     }
+        
+     quicksort(0, M-1);    /*   sort se au3ousa seira tis akmes */
+
      initialize(N); /*  Arxikopoihsh akmwn se adeio synolo */
-     for (i = 0; i < N - 1; i++)
-          for (j = 0; j < 3; j++)
-               F[i][j] = -1; /*  '-1' an empty */
-     while (edges < N - 1) {
+     while ((edges < N - 1) &&(next<M) ) {
           a = E[0][next];
           b = E[1][next];
           i = find(a);
           j = find(b);
           if (i!=j) {
-               merge(i, j);
-               F[edges][0] = E[0][next];
-               F[edges][1] = E[1][next];
-               F[edges][2] = E[2][next];
+               unite(i, j, E[2][next]);
+               weight += E[2][next];
+               E[3][next]=1;  // flag oti anikei sto MST
                edges++;
           }
           next++;
-     } /*  display edges comprising minimal spanning tree */
-     printf("\nMinimal Spanning Tree Edges:\n");
-     printf("F = (");
-     for (i = 0; i < N - 1; i++) {
-          printf("(V%ld, V%ld)",  F[i][0],  F[i][1]);
-          if (i < N - 2) printf(",  ");
-          weight = weight + F[i][2];
-     }
-     printf(")\n");
-     printf("Minimal Spanning Tree Weight = %lu\n",  weight);
+     } 
+     return weight;
 }
 
+long long min(long long a, long long b){
+     if (a < b){
+               return a;
+     }else{
+               return b;
+     }
+}
+
+long long second_mst(unsigned long long mst){
+     long i;
+     long long lemax=0;
+     long long lemin=-1;
+     for (i=0;i<M;i++){
+          if (E[3][i]!=1){    // If not in mst
+                if (E_M[E[0][i]] >= E_M[E[1][i]]){
+                     lemax=E_M[E[0][i]];
+                } else{
+                     lemax=E_M[E[1][i]];
+                }
+                if (lemin==-1){
+                          lemin=E[2][i]-lemax;
+                }else{
+                     lemin=min(lemin, (E[2][i]-lemax));
+                }
+                     
+          }
+     }
+     return lemin;
+}
 
 int main(){
-     long i, j, start, end;
+     long i, start, end;
      long value;
      /*  Input */
 	scanf("%lu",&N); //koryfes
 	scanf("%lu",&M); //akmes
 
      /* Allocations */
-     W = (long**) malloc( N * sizeof(long*));
-     for (i = 0; i < N; i++) {
-            W[i] = (long*)malloc(N * sizeof(long));
-     }
      U = (long*) malloc(N * sizeof(long));
+     E_M = (long*) malloc(N * sizeof(long));
      E[0]=(long*) malloc(M * sizeof(long));
      E[1]=(long*) malloc(M * sizeof(long));
      E[2]=(long*) malloc(M * sizeof(long));
-
+     E[3]=(long*) malloc(M * sizeof(long));
           
-     /* Initialization toy Adjacency Matrix */
-     for (i=0;i<N;i++){
-          for (j=0;j<N;j++){
-               W[i][j]=0;
-          }
-     }
-
      /*   Placement of Edges in Adjacency Matrix and Edges set   */
      for (i=0;i<M;i++){
-	     scanf("%lu",&start);
-	     scanf("%lu",&end);
-          scanf("%lu", &value);
-          W[start-1][end-1] = value;
-          W[end-1][start-1] = value;
-          E[0][i] = (long) start-1;
-          E[1][i] = (long) end-1;
+	     scanf("%ld",&start);
+	     scanf("%ld",&end);
+          scanf("%ld", &value);
+          E[0][i] = start-1;
+          E[1][i] = end-1;
           E[2][i] = value;
+          E[3][i] = 0;
      }
      
-     printf("\n");
-     for (i = 0; i < M; i++) {
-          printf(" %lu",  E[0][i]);
-          printf(" %lu",  E[1][i]);
-          printf(" %lu",  E[2][i]);
-          printf("\n");
-     }
-          kruskal();
-
+     unsigned long long mst=kruskal();
+     long long mst2=second_mst(mst);
+     printf("%llu %llu\n", mst, mst+mst2);
      return 0;     
 }
